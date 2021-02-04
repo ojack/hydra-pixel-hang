e231a2c570d25c3aec52cb852f725c51fbbdff58
olivia jack <rhizomaticode@gmail.com>
2021-02-04 17:42:53 +0100

    added windy map

---
+++ pastel-blob/older/crop3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="crop3" tilewidth="32" tileheight="32" tilecount="640" columns="32">
+ <image source="../../Pictures/crop3.png" trans="010208" width="1035" height="650"/>
+</tileset>
