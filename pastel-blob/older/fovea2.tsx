1821aac17e0175c19f2ee3990b6dbc5a57401512
ojack <rhizomaticode@gmail.com>
2021-02-04 16:45:39 +0000

    Deploying to gh-pages from master @ e231a2c570d25c3aec52cb852f725c51fbbdff58 🚀

---
+++ pastel-blob/older/fovea2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="fovea2" tilewidth="32" tileheight="32" tilecount="4558" columns="86">
+ <image source="../../Pictures/fovea2.png" trans="010208" width="2766" height="1698"/>
+</tileset>
---
+++ pastel-blob/older/test.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="test" tilewidth="32" tileheight="32" tilecount="665" columns="35">
+ <image source="../../Pictures/blob2.png" trans="010208" width="1134" height="633"/>
+</tileset>
